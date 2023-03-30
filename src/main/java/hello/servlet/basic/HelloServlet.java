7a251d6eda64ea35fdccb680c12967a8576ee95d
PyeonMinjun <alswns9810@gmail.com>
2023-03-30 23:11:34 +0900

    Hello 서블릿

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        // 헤더 정보에 들어감
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");  //인코딩 정보를 알려줌
+        response.getWriter().write("hello"+ username);
+
+
+    }
+}
