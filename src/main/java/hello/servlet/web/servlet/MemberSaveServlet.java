941d3816df768fb1a8540eaa32b95f594489f5f1
PyeonMinjun <alswns9810@gmail.com>
2023-04-05 20:22:02 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기

---
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,47 @@
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance(); // 저장하려면 멤버 리포지토리가 필요
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        데이터를 가져와서 읽으려면 get의 url의 쿼리스트링이던 html 폼데이터든 데이터를 읽어야 함
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age")); // request로 꺼내면 항상 string형이다 age이므로 int형으로 변환
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter w = response.getWriter();
+        w.write("<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                " <li>id="+member.getId()+"</li>\n" +
+                " <li>username="+member.getUsername()+"</li>\n" +
+                " <li>age="+member.getAge()+"</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
+                "</body>\n" +
+                "</html>");
+
+    }
+}
